2734b22636e3be6b10cf0e4cc257fa4b03a161a6
DerekToop <syfool@hotmail.com>
2019-06-23 21:46:57 +0930

    Looking fine, but actually not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormArray, FormGroup, Validators } from '@angular/forms';
+import { ICheckBoxItem } from '../models/checkbox-item.interface'
 
 @Component({
   selector: 'my-app',
@@ -8,14 +9,8 @@ import { FormBuilder, FormControl, FormArray, FormGroup, Validators } from '@ang
 })
 export class AppComponent  {
   name = 'Angular';
-
-  readonly Rules = [
-    'BD',
-    'PM',
-    'MW',
-    'PSC',
-    'PS',
-  ];
+  LookupTypes = [ 'BD', 'PM', 'MW', 'PSC', 'PS' ];
+  dbTypes = [ 'BD', 'PSC' ];
 
   constructor(
     private fb: FormBuilder,
@@ -23,24 +18,38 @@ export class AppComponent  {
   
   testForm = this.fb.group({
     title: [ 'Campbelltown' ],
-    rules: this.mapToCheckboxArrayGroup(this.Rules)
+    rules: this.fb.array(this.getTypesFormGroups())
   });
 
-  get formRules() {
-    return this.testForm.get('rules');
+  get rules() {
+    return this.testForm.get('rules') as FormArray;
   }
 
   ngOnInit() {
     
   }
 
-  private mapToCheckboxArrayGroup(data: string[]): FormArray {
-    return this.fb.array(data.map((i) => {
-      return this.fb.group({
-        name: i,
-        selected: false
-      });
-    }));
-}
+  getTypes(): ICheckBoxItem[] {
+    let checkboxes: ICheckBoxItem[] = [];
+    this.LookupTypes.forEach((type) => {
+      const item: ICheckBoxItem = {
+        id: type,
+        selected: this.dbTypes.indexOf(type) >= 0,
+        name: type
+      }
+      checkboxes.push(item);
+    });
+    return checkboxes;
+  }
 
+  getTypesFormGroups(): FormGroup[] {
+    const checkboxes = this.getTypes();
+    const groups: FormGroup[] = [];
+    checkboxes.forEach((box) => {
+      const group = this.fb.group(box);
+      groups.push(group);
+    });
+
+    return groups;
+  }
 }
